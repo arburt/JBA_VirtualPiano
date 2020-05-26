e1fc99653e9058e15ad78c3bd06766b32664ba4f
arburt <arburt@users.noreply.github.com>
2020-05-26 00:25:08 -0500

    Change OR to AND

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let audio;
 let validKeys = ["A", "S", "D", "F", "G", "H", "J", "W", "E", "T", "Y", "U"];
 
 function getKeyCode(key) {
-    if (typeof(key) == "string" || key.indexOf("Key", 1)) {
+    if (typeof(key) == "string" && key.indexOf("Key", 1)) {
         key = key.substr(3)
     }
 
