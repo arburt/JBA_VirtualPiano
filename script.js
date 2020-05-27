c2573fb22913f09fee69b6d0fc7a58a65aa2c4c9
arburt <arburt@users.noreply.github.com>
2020-05-27 01:35:03 -0500

    Don't reassign parameters and add semicolons

--- script.js
+++ script.js
@@ -1,23 +1,22 @@
 let key;
+let key2;
 let audio;
 let validKeys = ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'W', 'E', 'T', 'Y', 'U'];
 
 function getKeyCode(key) {
-    if (typeof(key) == 'string' && key.indexOf('Key', 1)) {
-        key = key.substr(3)
-    }
+    key2 = typeof(key) == 'string' && key.indexOf('Key', 1) ? key.substr(3) : null;
 
-    return key && key.length === 1 && validKeys.includes(key) ? key : null
+    return key2 && key2.length === 1 && validKeys.includes(key2) ? key2 : null;
 }
 
 document.addEventListener('keydown', function(event) {
     key = getKeyCode(event.code)
      if (key) {
-        audio = new Audio(`media/${key}.mp3`)
-        console.log(`The '${key}' key is pressed.`)
-        audio.play().then()
+        audio = new Audio(`media/${key}.mp3`);
+        console.log(`The '${key}' key is pressed.`);
+        audio.play().then();
     }
     else {
-         console.log('An invalid key was pressed.')
+         console.log('An invalid key was pressed.');
      }
-})
+});
