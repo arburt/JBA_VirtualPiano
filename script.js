0a38cc663e52f819b40a052fe467b51b087a6290
arburt <arburt@users.noreply.github.com>
2020-05-26 23:43:23 -0500

    Switch to single quotes

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 let key;
 let audio;
-let validKeys = ["A", "S", "D", "F", "G", "H", "J", "W", "E", "T", "Y", "U"];
+let validKeys = ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'W', 'E', 'T', 'Y', 'U'];
 
 function getKeyCode(key) {
-    if (typeof(key) == "string" && key.indexOf("Key", 1)) {
+    if (typeof(key) == 'string' && key.indexOf('Key', 1)) {
         key = key.substr(3)
     }
 
     return key && key.length === 1 && validKeys.includes(key) ? key : null
 }
 
-document.addEventListener("keydown", function(event) {
+document.addEventListener('keydown', function(event) {
     key = getKeyCode(event.code)
      if (key) {
         audio = new Audio(`media/${key}.mp3`)
@@ -18,6 +18,6 @@ document.addEventListener("keydown", function(event) {
         audio.play().then()
     }
     else {
-         console.log("An invalid key was pressed.")
+         console.log('An invalid key was pressed.')
      }
 })
